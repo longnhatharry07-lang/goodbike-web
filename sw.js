69fac427c78bc84374ebd9eddbf0f72fcc68fd55
longnhatharry07-lang <longnhatharry07@gmail.com>
2026-08-11 18:13:49 +0700

    Them so hieu ban + tu nhac cap nhat khi may chay ban cu

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker cho GOODBIKE PWA
 // Tăng số phiên bản này mỗi khi bạn cập nhật app để buộc làm mới cache.
-const VERSION = 'goodbike-v4';
+const VERSION = 'goodbike-2026-08-11-e';
 const APP_SHELL = [
   './',
   './index.html',
