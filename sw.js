d51953e84bab997e988f3c8bb303e17a20ca5fc5
longnhatharry07-lang <longnhatharry07@gmail.com>
2026-09-12 10:20:03 -0700

    Ban 2026-09-12-lienhe-10: bo khach quen & cong no khoi Thong ke KH lien he

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker cho GOODBIKE PWA
 // Tăng số phiên bản này mỗi khi bạn cập nhật app để buộc làm mới cache.
-const VERSION = 'goodbike-2026-09-12-lienhe-9';
+const VERSION = 'goodbike-2026-09-12-lienhe-10';
 const APP_SHELL = [
   './',
   './index.html',
